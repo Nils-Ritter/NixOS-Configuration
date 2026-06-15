46d3f9c7d0a26242a0ac953497089ad7fedea5b6
= <=>
2026-06-15 11:13:18 +0200

    nvim_config_change

--- dots/nvim/init.lua
+++ dots/nvim/init.lua
@@ -534,6 +534,7 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         -- You can add other tools here that you want Mason to install
         rust_analyzer,
+        jdtls,
       })
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
